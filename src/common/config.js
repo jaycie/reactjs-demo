6f1f78611e87a7babe0506a66ca3ad3b500f7935
f2e <xiezhanggen@gmail.com>
2017-04-21 16:24:26 +0800

    add address

--- src/common/config.js
+++ src/common/config.js
@@ -1,7 +1,8 @@
 // export const AJAXHOST = 'http://doc.freeh5.com/';
 // export const AJAXHOST = 'http://m.com/';
-// export const AJAXHOST = 'http://jaycie.com/qingniao8.com/api/index.php/';
-// export const STATICHOST = 'http://jaycie.com/qingniao8.com/micro/data/upload/product/';
 
-export const AJAXHOST = 'http://api.freeh5.com/index.php/';
-export const STATICHOST = 'http://micro.freeh5.com/data/upload/product/';
+export const AJAXHOST = 'http://jaycie.com/qingniao8.com/api/index.php/';
+export const STATICHOST = 'http://jaycie.com/qingniao8.com/micro/data/upload/product/';
+
+// export const AJAXHOST = 'http://api.freeh5.com/index.php/';
+// export const STATICHOST = 'http://micro.freeh5.com/data/upload/product/';
--- src/pages/address.js
+++ src/pages/address.js
@@ -11,18 +11,63 @@ export default class Address extends Component {
     super(props);
   
     this.state = {
-      
+      realname: '',
+      detail_address: '',
+      tel: ''
     };
   }
 
   render() {
     return (
       <div className="address">
-        address
+        <div className="select">
+          <select name="province" id="province" onChange={this.selectCity.bind(this)}>
+            <option value="0">请选择省份</option>
+            <option value="1">广东省</option>
+            <option value="2">浙江省</option>
+            <option value="3">江西省</option>
+          </select>
+        </div>
+        <div className="select">
+          <select name="city" id="city">
+            <option value="0">请选择城市</option>
+            <option value="1">广东省</option>
+            <option value="2">浙江省</option>
+            <option value="3">江西省</option>
+          </select>
+        </div>
+        <div className="select">
+          <select name="area" id="area">
+            <option value="0">请选择区县</option>
+            <option value="1">广东省</option>
+            <option value="2">浙江省</option>
+            <option value="3">江西省</option>
+          </select>
+        </div>
+        <div className="select">
+          <input type="text" name="detail_address" placeholder="请输入详细地址" onChange={(e)=>this.setState({detail_address: e.target.value})} />
+        </div>
+        <div className="select">
+          <input type="text" name="realname" placeholder="请输入收货人姓名" onChange={(e)=>this.setState({realname: e.target.value})}/>
+        </div>
+        <div className="select">
+          <input type="number" name="tel" maxLength="11" placeholder="请输入收货人手机号码" onChange={(e)=>this.setState({tel: e.target.value})}/>
+        </div>
+
+        <div className="bottom-button" onClick={this.save.bind(this)}>保存地址</div>
       </div>
     );
   }
 
+  save() {
+
+  }
+
+  selectCity = (e)=> {
+    console.log(e.target);
+    console.log(e.target.value);
+    console.log(e.target.text);
+  }
 
   componentDidMount(){
     
