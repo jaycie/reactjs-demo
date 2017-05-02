01536daf8941fab059bf503ccac9a150f1923939
Your Name <you@example.com>
2017-05-02 14:00:07 +0800

    reactjs

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -14,30 +14,31 @@ export default class Detail extends Component {
 		this.state = {
 			productId: this.props.match.params.productId,
 			data: '',
-			left:'left',
-			arrays:[],
-			id:0 //选中的第几张
+			left: 'left',
+			arrays: [],
+			id: 0 //选中的第几张
 		};
 	}
 
 	render() {
-			const Li=this.state.arrays.map((value,index)=>{
-				let cls= index===this.state.id?'color':'';
-				return(<li key={index} className={cls} onClick={this.Carousel.bind(this,index)}></li>)
-			})
+		const Li = this.state.arrays.map((value, index) => {
+			let cls = index === this.state.id ? 'color' : '';
+			return(<span key={index} className={cls} onClick={this.Carousel.bind(this,index)}></span>)
+		})
 		return(
 			<div className="detail">
         <div className="detail-title">
           <div className="big-pic">
-          	<div id="photograph" ref='myimg'>
-            	<span><img src={`${STATICHOST}${this.state.data.image}`} alt={this.state.data.title} /></span>
-            	<span><img src={`${STATICHOST}${this.state.data.image}`} alt={this.state.data.title} /></span>
-            	<span><img src={`${STATICHOST}${this.state.data.image}`} alt={this.state.data.title} /></span>
-            	<span><img src={`${STATICHOST}${this.state.data.image}`} alt={this.state.data.title} /></span>
+          	<div id="photograph" ref='myimg' style={this.state.width}>
+            <span><img src={`${STATICHOST}${this.state.data.image}`} alt={this.state.data.title} style={this.state.imgwidth} /></span>
+            {this.state.arrayss}
             </div>
-            <ul className="nav">
-				{Li}
-			</ul>
+            <div className="nav" style={this.state.bom}>
+            	<div>
+            	{Li}
+            	</div>
+				
+			</div>
             <p>{this.state.data.title}</p>
           </div>
           <div className="show-price">
@@ -52,7 +53,7 @@ export default class Detail extends Component {
         </div>
 
         <div className="detail-button">
-          <div className='collect' onClick={this.collect.bind(this)} style={{color: '#333'}}>收藏</div>
+          <div className='collect' onClick={this.collect.bind(this)}>收藏</div>
           <div className="shop-car" onClick={this.addCar.bind(this)}>加入购物车</div>
           <div className="buy" onClick={this.buy.bind(this)}>立即购买</div>
         </div>
@@ -65,16 +66,14 @@ export default class Detail extends Component {
 	}
 
 	collect(event) {
-		if(event.target.style.color === 'rgb(51, 51, 51)') {
-			alert('已收藏')
-			event.target.style = 'color:red'
-		} else {
-			alert('取消收藏')
-			event.target.style = 'color:#333'
-		}
 		const url1 = AJAXHOST + 'mall/follow/' + localStorage.uid + '/' + this.state.productId;
 		corsPostFetch(url1).then(objt => {
 			console.log(objt)
+			if(objt.code === 200) {
+				alert('你已关注')
+			} else {
+				alert('取消关注')
+			}
 		})
 	}
 
@@ -102,30 +101,54 @@ export default class Detail extends Component {
 
 	photograph() {
 		let span = this.refs.myimg.childNodes;
+		console.log(span)
 		let arrays = [];
+		this.setState({
+			width: {
+				width: span.length * 100 + '%'
+			}
+		})
 		for(let i = 0; i < span.length; i++) {
-			arrays.push(span[i])
+			arrays.push(span[i]);
 			span[i].setAttribute(this.state.left, span[i].offsetLeft)
 		}
-  		this.setState({
-  			arrays:arrays
-  		})
+		this.setState({
+			arrays: arrays
+		});
 	}
-	Carousel(pid){
+	Carousel(pid) {
 		let span = this.refs.myimg.childNodes;
-		this.setState({id:pid});
-		this.refs.myimg.style='margin-left:-'+span[pid].getAttribute(this.state.left)+'px'
+		console.log(span.length * 100)
+		this.setState({
+			id: pid
+		});
+		this.refs.myimg.style = 'margin-left:-' + span[pid].getAttribute(this.state.left) + 'px;width:' + (span.length * 100) + '%'
 	}
 	componentDidMount() {
 		const url = AJAXHOST + 'mall/product_detail/' + this.state.productId;
 		corsPostFetch(url).then(obj => {
 			if(obj.code === 200) {
-				this.setState({
-					data: obj.data[0]
+				let imgwidth = obj.data[0].images === null ? {
+					width: '100%'
+				} : {
+					width: 100 / (obj.data[0].images.split(',').length + 1) + '%'
+				}
+				let arry = obj.data[0].images === null ? '' : obj.data[0].images.split(',').map((value, index) => {
+					return(<span key={index}><img src={`${STATICHOST}${value}`} alt={this.state.data.title} style={imgwidth}/></span>)
 				});
+				this.setState({
+					data: obj.data[0],
+					arrayss: arry,
+					imgwidth: imgwidth
+				});
+				if(obj.data[0].images !== null) {
+					this.photograph()
+					this.setState({
+						bom:{marginTop:'-1rem'}
+					})
+				}
 			}
 		});
-		this.photograph()
 	}
 	res() {
 		localStorage.setItem('id' + JSON.parse(localStorage.getItem('order')).id + '', localStorage.getItem('order'))
