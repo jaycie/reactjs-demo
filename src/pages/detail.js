2a3f8df4ea0646e96783d13e62c7466a527bb463
Your Name <you@example.com>
2017-05-04 17:15:37 +0800

    reactjs

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -33,7 +33,6 @@ export default class Detail extends Component {
 		})
 		return(
 			<div className="detail">
-			112
         <div className="detail-title">
           <div className="big-pic" onTouchStart={this.Carousel.bind(this)} onTouchMove={this.glide.bind(this)} onTouchEnd={this.lift.bind(this)}>
           	<div id="photograph" ref='myimg' style={this.state.width}>
